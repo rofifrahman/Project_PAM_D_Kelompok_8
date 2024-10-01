e54992055ce13a54cf6f6c376501faf74978af1b
rofifrahman_ <rofifrahmanfuadi04@gmail.com>
2024-10-01 22:21:07 +0700

    Intent Membuat Lowongan Pekerjaan

--- app/src/main/java/com/example/freels/MainActivity.java
+++ app/src/main/java/com/example/freels/MainActivity.java
@@ -1,18 +1,45 @@
 package com.example.freels;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_membuat_lowongan_pekerjaan);
+        setContentView(R.layout.activity_main);
+
+        TextView namaPerusahaan = findViewById(R.id.namaPerusahaan);
+        TextView pekerjaan = findViewById(R.id.pekerjaan);
+        TextView lokasi = findViewById(R.id.lokasi);
+        TextView jenisPekerjaan = findViewById(R.id.jenisPekerjaan);
+        TextView gajiMinimum = findViewById(R.id.gajiMinimum);
+        TextView gajiMaksimum = findViewById(R.id.gajiMaksimum);
+        TextView ringkasanPekerjaan = findViewById(R.id.ringkasanPekerjaan);
+        TextView kualfikasiPekerjaan = findViewById(R.id.kualifikasiPekerjaan);
+
+        Intent i = getIntent();
+
+        String teksNamaPerusahaan = i.getStringExtra("namaPerusahaan");
+        String teksPekerjaan = i.getStringExtra("pekerjaan");
+        String teksLokasi = i.getStringExtra("lokasi");
+        String teksJenisPekerjaan = i.getStringExtra("jenisPekerjaan");
+        String teksGajiMinimum = i.getStringExtra("gajiMinimum");
+        String teksGajiMaksimum = i.getStringExtra("gajiMaksimum");
+        String teksRingkasanPekerjaan = i.getStringExtra("ringkasanPekerjaan");
+        String teksKualifikasiPekerjaan = i.getStringExtra("kualifikasiPekerjaan");
+
+        namaPerusahaan.setText(teksNamaPerusahaan);
+        pekerjaan.setText(teksPekerjaan);
+        lokasi.setText(teksLokasi);
+        jenisPekerjaan.setText(teksJenisPekerjaan);
+        gajiMinimum.setText(teksGajiMinimum);
+        gajiMaksimum.setText(teksGajiMaksimum);
+        ringkasanPekerjaan.setText(teksRingkasanPekerjaan);
+        kualfikasiPekerjaan.setText(teksKualifikasiPekerjaan);
     }
 }
--- app/src/main/java/com/example/freels/MembuatLowonganPekerjaan.java
+++ app/src/main/java/com/example/freels/MembuatLowonganPekerjaan.java
@@ -1,18 +1,70 @@
 package com.example.freels;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MembuatLowonganPekerjaan extends AppCompatActivity {
+public class MembuatLowonganPekerjaan extends AppCompatActivity implements View.OnClickListener {
+
+
+    private EditText namaPerusahaan;
+    private EditText pekerjaan;
+    private EditText lokasi;
+    private EditText jenisPekerjaan;
+    private EditText gajiMinimum;
+    private EditText gajiMaksimum;
+    private EditText ringkasanPekerjaan;
+    private EditText kualifikasiPekerjaan;
+    private Button btUnggahLowongan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_membuat_lowongan_pekerjaan);
+
+        this.namaPerusahaan = this.findViewById(R.id.InputNamaPerusahaan);
+        this.pekerjaan = this.findViewById(R.id.InputPekerjaan);
+        this.lokasi = this.findViewById(R.id.InputLokasi);
+        this.jenisPekerjaan = this.findViewById(R.id.InputJenisPekerjaan);
+        this.gajiMinimum = this.findViewById(R.id.InputGajiMinimum);
+        this.gajiMaksimum = this.findViewById(R.id.InputGajiMaksimum);
+        this.ringkasanPekerjaan = this.findViewById(R.id.InputRingkasanPekerjaan);
+        this.kualifikasiPekerjaan = this.findViewById(R.id.InputKualifikasiPekerjaan);
+        this.btUnggahLowongan = this.findViewById(R.id.UnggahLowongan);
+
+        this.btUnggahLowongan.setOnClickListener(MembuatLowonganPekerjaan.this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i = new Intent(this, MainActivity.class);
+        String teksNamaPerusahaan = this.namaPerusahaan.getText().toString();
+        i.putExtra("namaPerusahaan", teksNamaPerusahaan);
+
+        String teksPekerjaan = this.pekerjaan.getText().toString();
+        i.putExtra("pekerjaan", teksPekerjaan);
+
+        String teksLokasi = this.lokasi.getText().toString();
+        i.putExtra("lokasi", teksLokasi);
+
+        String teksJenisPekerjaan = this.jenisPekerjaan.getText().toString();
+        i.putExtra("jenisPekerjaan", teksJenisPekerjaan);
+
+        String teksGajiMinimum = this.gajiMinimum.getText().toString();
+        i.putExtra("gajiMinimum", teksGajiMinimum);
+
+        String teksGajiMaksimum = this.gajiMaksimum.getText().toString();
+        i.putExtra("gajiMaksimum", teksGajiMaksimum);
+
+        String teksRingkasanPekerjaan = this.ringkasanPekerjaan.getText().toString();
+        i.putExtra("ringkasanPekerjaan", teksRingkasanPekerjaan);
+
+        String teksKualifikasiPekerjaan = this.kualifikasiPekerjaan.getText().toString();
+        i.putExtra("kualifikasiPekerjaan", teksKualifikasiPekerjaan);
+        startActivity(i);
     }
 }
